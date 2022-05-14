cff0aed7f2710d1c28e6542c538f35eb4691cf48
Sergio Guaka <scm0@correo.ugr.es>
2022-05-14 10:47:47 +0200

    Fix syntax

--- keyboards/crkbd/keymaps/sergiocampoy/keymap.c
+++ keyboards/crkbd/keymaps/sergiocampoy/keymap.c
@@ -16,7 +16,7 @@ enum layers {
 };
 
 // Tap Dance definitions
-qt_tap_dance_action_t tap_dance_actions[] = {
+qk_tap_dance_action_t tap_dance_actions[] = {
     // Tap to use space, hold to activate Symbol layer
     [TD_MO_SPACE] = ACTION_TAP_DANCE_LAYER_MOVE(KC_SPACE, _SYMBOL),
 
@@ -37,14 +37,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * SPC & RET act as layer modifiers when held
  */
 [_COLEMAK_DH] = LAYOUT_split_3x6_3(
-KC_NO,       KC_Q,        KC_W,       KC_F,       KC_P,  KC_B,   KC_J,       KC_L,        KC_U,        KC_Y,        KC_CSLN, KC_NO,
+KC_NO,       KC_Q,        KC_W,       KC_F,       KC_P,  KC_B,   KC_J,       KC_L,        KC_U,        KC_Y,        KC_SCLN, KC_NO,
 KC_NO,RGUI_T(KC_A),LALT_T(KC_R),CTL_T(KC_S),SFT_T(KC_T), KC_G,   KC_M,RSFT_T(KC_N),RCTL_T(KC_E),RALT_T(KC_I),RGUI_T(KC_O),   KC_NO,
 KC_NO,       KC_Z,        KC_X,       KC_C,       KC_D,  KC_V,   KC_K,       KC_H,        KC_COMM,     KC_DOT,      KC_SLSH, KC_NO,
 
 // KC_NO, MO(_SYMBOL), KC_SPACE,   KC_RETURN, MO(_FUNCTION), KC_NO
 KC_NO, TD(TD_MO_SPACE), KC_ESC,   KC_BSPC, TD(TD_MO_ENTER), KC_NO
+),
 
-,./<>?
 /*
  *    |-Symbol Layer-----------------------------------------------------|
  *    |  1  |  2  |  3  |  4  |  5  |      |  6  |  7  |  8  |  9  |  0  |
@@ -54,13 +54,12 @@ KC_NO, TD(TD_MO_SPACE), KC_ESC,   KC_BSPC, TD(TD_MO_ENTER), KC_NO
  *                      |-----------|      |-----------|
  * SPC & RET act as layer modifiers when held
  */
-[_COLEMAK_DH] = LAYOUT_split_3x6_3(
+[_SYMBOL] = LAYOUT_split_3x6_3(
 KC_NO,       KC_1,        KC_2,       KC_3,       KC_4,  KC_5,   KC_6,       KC_7,        KC_8,        KC_9,        KC_0,    KC_NO,
 KC_NO,RGUI_T(KC_A),LALT_T(KC_R),CTL_T(KC_S),SFT_T(KC_T), KC_G,   KC_M,RSFT_T(KC_N),RCTL_T(KC_E),RALT_T(KC_I),RGUI_T(KC_O),   KC_NO,
 KC_NO,       KC_Z,        KC_X,       KC_C,       KC_D,  KC_V,   KC_K,       KC_H,        KC_COMM,     KC_DOT,      KC_SLSH, KC_NO,
 
 // KC_NO, MO(_SYMBOL), KC_SPACE,   KC_RETURN, MO(_FUNCTION), KC_NO
 KC_NO, TD(TD_MO_SPACE), KC_ESC,   KC_BSPC, TD(TD_MO_ENTER), KC_NO
-),
-
+)
 };
--- keyboards/crkbd/keymaps/sergiocampoy/rules.mk
+++ keyboards/crkbd/keymaps/sergiocampoy/rules.mk
@@ -1,5 +1,8 @@
-MOUSEKEY_ENABLE = yes    # Mouse keys
-RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
-OLED_ENABLE     = yes
-OLED_DRIVER     = SSD1306
-LTO_ENABLE      = yes
+# MOUSEKEY_ENABLE = yes    # Mouse keys
+# RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+# OLED_ENABLE     = yes
+# OLED_DRIVER     = SSD1306
+# LTO_ENABLE      = yes
+
+# Enable tap dance
+TAP_DANCE_ENABLE = yes
